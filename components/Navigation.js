5b3eacdb2da24133d8b9e0d7ea1a2f4d64595f4d
Ian Fajardo <ianfajardozx@gmail.com>
2021-10-28 00:21:41 -0400

    init commit

---
+++ components/Navigation.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+//import { Link } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar"
+import Nav from "react-bootstrap/Nav";
+
+import Container from "react-bootstrap/Container";
+//import logo from "../IFblack.png";
+
+export default class Navigation extends Component {
+  render() {
+    return (
+      <Navbar bg="darkpurp" variant="dark" expand="lg">
+        <Container>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="#work">Work With Me</Nav.Link>
+              <Nav.Link target="_blank" href="https://www.behance.net/ianfajardo">Portfolio</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    );
+  }
+}
